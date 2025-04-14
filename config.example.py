688d6b862e61b5f20465f708d0919f4722a3be57
artemkiruhin <temkaunik@gmail.com>
2025-04-14 21:09:03 +0300

    add: session for queries

--- config.example.py
+++ config.example.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "db_driver://db_username:db_password@host_address:db_port/db_name"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
