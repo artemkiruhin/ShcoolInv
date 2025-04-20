3340b5750b1c01c4d151dd0368a407b776c34ff7
artemkiruhin <temkaunik@gmail.com>
2025-04-20 15:14:25 +0300

    add: DI

---
+++ backend/dependency_injection.py
@@ -0,0 +1,112 @@
+from dependency_injector import containers, providers
+from repositories import (
+    UserRepository,
+    RoomRepository,
+    InventoryItemRepository,
+    InventoryCategoryRepository,
+    InventoryConditionRepository,
+    LogRepository
+)
+from services import (
+    UserService,
+    RoomService,
+    InventoryItemService,
+    InventoryCategoryService,
+    InventoryConditionService,
+    LogService
+)
+from config import SessionLocal
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        modules=[
+            "main"
+        ]
+    )
+
+    db_session = providers.Singleton(SessionLocal)
+
+    user_repository = providers.Factory(
+        UserRepository,
+        session=db_session
+    )
+
+    room_repository = providers.Factory(
+        RoomRepository,
+        session=db_session
+    )
+
+    inventory_item_repository = providers.Factory(
+        InventoryItemRepository,
+        session=db_session
+    )
+
+    inventory_category_repository = providers.Factory(
+        InventoryCategoryRepository,
+        session=db_session
+    )
+
+    inventory_condition_repository = providers.Factory(
+        InventoryConditionRepository,
+        session=db_session
+    )
+
+    log_repository = providers.Factory(
+        LogRepository,
+        session=db_session
+    )
+
+    user_service = providers.Factory(
+        UserService,
+        user_repository=user_repository
+    )
+
+    room_service = providers.Factory(
+        RoomService,
+        room_repository=room_repository
+    )
+
+    inventory_item_service = providers.Factory(
+        InventoryItemService,
+        inventory_item_repository=inventory_item_repository,
+        condition_repository=inventory_condition_repository,
+        category_repository=inventory_category_repository,
+        room_repository=room_repository
+    )
+
+    inventory_category_service = providers.Factory(
+        InventoryCategoryService,
+        inventory_category_repository=inventory_category_repository
+    )
+
+    inventory_condition_service = providers.Factory(
+        InventoryConditionService,
+        inventory_condition_repository=inventory_condition_repository
+    )
+
+    log_service = providers.Factory(
+        LogService,
+        log_repository=log_repository
+    )
+
+
+container = Container()
+
+def get_user_service() -> UserService:
+    return container.user_service()
+
+def get_room_service() -> RoomService:
+    return container.room_service()
+
+def get_inventory_item_service() -> InventoryItemService:
+    return container.inventory_item_service()
+
+def get_inventory_category_service() -> InventoryCategoryService:
+    return container.inventory_category_service()
+
+def get_inventory_condition_service() -> InventoryConditionService:
+    return container.inventory_condition_service()
+
+def get_log_service() -> LogService:
+    return container.log_service()
