dbfbf2fa0be5f2a823359a0b051956f040a6bf3c
artemkiruhin <temkaunik@gmail.com>
2025-04-28 21:54:53 +0300

    upd: header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
@@ -6,15 +7,29 @@ const Header = () => {
             <div className={styles.logoContainer}>
                 <div className={styles.logoIcon}>
                     <svg viewBox="0 0 24 24">
-                        <path d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z" />
+                        <path d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z"/>
                     </svg>
                 </div>
                 <h1>School<span>Inv</span></h1>
             </div>
             <nav>
                 <ul>
-                    <li><a href="#" className={styles.active}>Главная</a></li>
-                    <li><a href="#">Инвентарь</a></li>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? styles.active : ''}
+                        >
+                            Главная
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/inventory"
+                            className={({ isActive }) => isActive ? styles.active : ''}
+                        >
+                            Инвентарь
+                        </NavLink>
+                    </li>
                     <li><a href="#">Отчеты</a></li>
                     <li><a href="#">Настройки</a></li>
                 </ul>
