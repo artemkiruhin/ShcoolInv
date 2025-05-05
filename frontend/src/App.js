52bda990e7732302bb921c9f7b082da6036ff26b
artemkiruhin <temkaunik@gmail.com>
2025-05-05 21:48:20 +0300

    add: routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,60 @@
-import { useEffect } from 'react';
+import "../src/assets/styles/global.css"
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Inventory from './pages/Inventory/Inventory';
-import Footer from './components/Footer/Footer';
-import './styles/global.css';
-import './styles/variables.css';
-import './styles/animations.css';
-import InventoryItemPage from "./pages/InventoryItemPage/InventoryItemPage";
-import UserProfile from "./pages/UserProfile/UserProfile";
-import Header from "./components/Header/Header";
-import Users from "./pages/Users/Users";
-import UserPage from "./pages/UserPage/UserPage";
-import Rooms from "./pages/Rooms/Rooms";
-import RoomPage from "./pages/RoomPage/RoomPage";
-import Consumables from "./pages/ConsumablesPage/Consumables";
-import ConsumableDetails from "./pages/ConsumableDetails/ConsumableDetails";
+import Layout from './components/layout/Layout';
+import Dashboard from './components/pages/Dashboard';
+import InventoryList from './components/pages/Inventory/InventoryList';
+import InventoryItemForm from './components/pages/Inventory/InventoryItemForm';
+import InventoryView from './components/pages/Inventory/InventoryView';
+import CategoriesList from './components/pages/Categories/CategoriesList';
+import CategoryForm from './components/pages/Categories/CategoriesForm';
+import ConsumablesList from './components/pages/Consumables/ConsumablesList';
+import ConsumableForm from './components/pages/Consumables/ConsumablesForm';
+import RoomsList from './components/pages/Rooms/RoomsList';
+import RoomForm from './components/pages/Rooms/RoomsForm';
+import UsersList from './components/pages/Users/UsersList';
+import UserForm from './components/pages/Users/UsersForm';
+import LogsList from './components/pages/Logs/LogsList';
+import './index.css';
+
 
 function App() {
-    useEffect(() => {
-        document.body.style.opacity = '0';
-        document.body.style.transition = 'opacity 0.3s ease';
-
-        const timer = setTimeout(() => {
-            document.body.style.opacity = '1';
-        }, 100);
-
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
         <Router>
-            <div className="App">
-                <main>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/inventory" element={<Inventory />} />
-                        <Route path="/items/:itemId" element={<InventoryItemPage />} />
-                        <Route path="/profile" element={<UserProfile isAdmin={true} />} />
-                        <Route path="/users" element={<Users />} />
-                        <Route path="/users/:id" element={<UserPage />} />
-                        <Route path="/rooms" element={<Rooms />} />
-                        <Route path="/rooms/:roomId" element={<RoomPage />} />
-                        <Route path="/consumables" element={<Consumables />} />
-                        <Route path="/consumables/:id" element={<ConsumableDetails />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+
+                    {/* Inventory Routes */}
+                    <Route path="/inventory" element={<InventoryList />} />
+                    <Route path="/inventory/new" element={<InventoryItemForm />} />
+                    <Route path="/inventory/:id" element={<InventoryView />} />
+                    <Route path="/inventory/:id/edit" element={<InventoryItemForm />} />
+
+                    {/* Categories Routes */}
+                    <Route path="/categories" element={<CategoriesList />} />
+                    <Route path="/categories/new" element={<CategoryForm />} />
+                    <Route path="/categories/:id/edit" element={<CategoryForm />} />
+
+                    {/* Consumables Routes */}
+                    <Route path="/consumables" element={<ConsumablesList />} />
+                    <Route path="/consumables/new" element={<ConsumableForm />} />
+                    <Route path="/consumables/:id/edit" element={<ConsumableForm />} />
+
+                    {/* Rooms Routes */}
+                    <Route path="/rooms" element={<RoomsList />} />
+                    <Route path="/rooms/new" element={<RoomForm />} />
+                    <Route path="/rooms/:id/edit" element={<RoomForm />} />
+
+                    {/* Users Routes */}
+                    <Route path="/users" element={<UsersList />} />
+                    <Route path="/users/new" element={<UserForm />} />
+                    <Route path="/users/:id/edit" element={<UserForm />} />
+
+                    {/* Logs Route */}
+                    <Route path="/logs" element={<LogsList />} />
+                </Routes>
+            </Layout>
         </Router>
     );
 }
