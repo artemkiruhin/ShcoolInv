fa37ace012485fc372eab527acd42355f10b957e
artemkiruhin <temkaunik@gmail.com>
2025-05-05 21:46:26 +0300

    add: layout component

---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+const Layout = ({ children }) => {
+    return (
+        <div className="app-layout">
+            <Header />
+            <main className="main-content">
+                <div className="content-container">{children}</div>
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
