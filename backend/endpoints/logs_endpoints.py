40d65dc5b7c166fbc894afa5d9b935c6d5e8deed
artemkiruhin <temkaunik@gmail.com>
2025-05-05 16:35:28 +0300

    upd: _

--- backend/endpoints/logs_endpoints.py
+++
@@ -1,6 +0,0 @@
-from backend.configurations.flask_utils import authorized, Response200
-
-@authorized
-def get_logs(service):
-    logs = service.get_all()
-    return Response200.send(data=[log.__dict__ for log in logs])
